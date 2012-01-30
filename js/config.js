6d8caa40b19f5f2f44e7e55c6114b9a401ce5534
Pete LePage <pete.lepage@pobox.com>
2012-01-30 09:08:24 -0800

    First checkin

---
+++ js/config.js
@@ -0,0 +1,33 @@
+window.slidesConfig = window.slidesConfig || {
+  // Slide settings
+  settings : {
+    useBuilds: true,
+    showHiddenSlides: false
+  },
+  info: {
+    // Presentation Name
+    title: 'Presentation Title',
+    subtitle: 'Subtitle',
+
+    // Personal info
+    name: 'Presenter Name',
+    pic: './images/html5-guy.png',
+    gplus: 'https://plus.google.com/106189723444098348646',
+    twitter: 'chromiumdev',
+    blog: 'http://updates.html5rocks.com',
+
+    // Google Analytics Token
+    ga_account: 'UA-XXXXXXX-X',
+
+    // Links
+    slides: 'http://url_to_slides',
+    feedback: 'http://url_to_feedback',
+    location: 'Mountain View, CA',
+
+    // Presentation Timer
+    date: new Date('Jan 18 2012 14:30'),
+    logo: './images/html5_logo_256.png',
+    minutes: 60,
+    warnAt: 10
+  }
+};
